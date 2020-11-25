713e3cc0c3026e272cc76bc0045b727d8b92cefe
Ondřej Mikulenka <228580@vutbr.cz>
2020-11-25 16:32:44 +0100

    Hodil jsem tu motor

--- controllers/Mech_Project_Zdenek_Mikulenka/Mech_Project_Zdenek_Mikulenka.m
+++ controllers/Mech_Project_Zdenek_Mikulenka/Mech_Project_Zdenek_Mikulenka.m
@@ -1,9 +1,11 @@
 
 
 TIME_STEP = 64;
-
+  Max_S = 8;
+Left_M = wb_robot_get_device('Left_Motor');
 while wb_robot_step(TIME_STEP) ~= -1
-
+wb_motor_set_position(Left_M, inf);
+wb_motor_set_velocity(Left_M, Max_S);
   drawnow;
   
 end
