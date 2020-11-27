32bbe79687f92230403f589771acd08d0b414da6
Ondřej Mikulenka <228580@vutbr.cz>
2020-11-27 17:28:52 +0100

    Hexapod 3 legs
    zvednuty 3 nohy (polovina ke kroku).
    Dále nastane pokus o vytvoření funkce chůze vpřed.

--- controllers/Mech_Project_Zdenek_Mikulenka/Mech_Project_Zdenek_Mikulenka.m
+++ controllers/Mech_Project_Zdenek_Mikulenka/Mech_Project_Zdenek_Mikulenka.m
@@ -43,12 +43,31 @@ pause(2);
 wb_motor_set_position(Right_Rear_Shdr, 1);
 wb_motor_set_velocity(Right_Rear_Shdr, Max_S);
 
-wb_motor_set_position(Right_Rear_Base, -0.35);
+wb_motor_set_position(Right_Rear_Base, 0.55);
 wb_motor_set_velocity(Right_Rear_Base, Max_S);
 
 wb_motor_set_position(Right_Rear_Knee, -2);
 wb_motor_set_velocity(Right_Rear_Knee, Max_S);
 
+pause(2);
+wb_motor_set_position(Right_Front_Shdr, 1);
+wb_motor_set_velocity(Right_Front_Shdr, Max_S);
+
+wb_motor_set_position(Right_Front_Base, 0.55);
+wb_motor_set_velocity(Right_Front_Base, Max_S);
+
+wb_motor_set_position(Right_Front_Knee, -2);
+wb_motor_set_velocity(Right_Front_Knee, Max_S);
+
+pause(2);
+wb_motor_set_position(Left_Mid_Shdr, 1);
+wb_motor_set_velocity(Left_Mid_Shdr, Max_S);
+
+wb_motor_set_position(Left_Mid_Base, -0.55);
+wb_motor_set_velocity(Left_Mid_Base, Max_S);
+
+wb_motor_set_position(Left_Mid_Knee, -2);
+wb_motor_set_velocity(Left_Mid_Knee, Max_S);
 while wb_robot_step(TIME_STEP) ~= -1
 
   drawnow;
