94957f2a253ae82ab4d657d2b9fc585d3eb083af
maceska13 <m.zdenek@wing.cz>
2020-11-27 13:57:11 +0100

    Hexapod first movement
    první pohyby nohou a experimentální zadefinování nohou (zakomentováno)

--- controllers/Mech_Project_Zdenek_Mikulenka/Mech_Project_Zdenek_Mikulenka.m
+++ controllers/Mech_Project_Zdenek_Mikulenka/Mech_Project_Zdenek_Mikulenka.m
@@ -1,11 +1,16 @@
 
 
 TIME_STEP = 64;
-  Max_S = -8;
+  Max_S = 1;
+% offset Base f, m, r [rad] = 0.6, 0, -0.6 
+% offset Shdr u, d [rad] = 1, 0.8  
+% offset Knee u, d [rad] = -2.4, 0.4   
+  
 %Right Rear Leg Indexing
 Right_Rear_Base = wb_robot_get_device('RPC');
 Right_Rear_Shdr = wb_robot_get_device('RPF');
 Right_Rear_Knee = wb_robot_get_device('RPT');
+
 %Right Middle Leg Indexing
 Right_Mid_Base = wb_robot_get_device('RMC');
 Right_Mid_Shdr = wb_robot_get_device('RMF');
@@ -27,15 +32,25 @@ Left_Front_Base = wb_robot_get_device('LAC');
 Left_Front_Shdr = wb_robot_get_device('LAF');
 Left_Front_Knee = wb_robot_get_device('LAT');
 
+%Legs = [wb_robot_get_device('RPC'), wb_robot_get_device('RPF'), wb_robot_get_device('RPT');...
+%        wb_robot_get_device('RMC'), wb_robot_get_device('RMF'), wb_robot_get_device('RMT');...
+%        wb_robot_get_device('RAC'), wb_robot_get_device('RAF'), wb_robot_get_device('RAT');...
+%        wb_robot_get_device('LPC'), wb_robot_get_device('LPF'), wb_robot_get_device('LPT');...                                                                                       ];
+%        wb_robot_get_device('LMC'), wb_robot_get_device('LMF'), wb_robot_get_device('LMT');...
+%        wb_robot_get_device('LAC'), wb_robot_get_device('LAF'), wb_robot_get_device('LAT')];
+
+pause(2);
+wb_motor_set_position(Right_Rear_Shdr, 1);
+wb_motor_set_velocity(Right_Rear_Shdr, Max_S);
+
+wb_motor_set_position(Right_Rear_Base, -0.35);
+wb_motor_set_velocity(Right_Rear_Base, Max_S);
+
+wb_motor_set_position(Right_Rear_Knee, -2);
+wb_motor_set_velocity(Right_Rear_Knee, Max_S);
+
 while wb_robot_step(TIME_STEP) ~= -1
-%wb_motor_set_position(Left_M, inf);
-%wb_motor_set_velocity(Left_M, Max_S);
-%wb_motor_set_position(Right_M, inf);
-%wb_motor_set_velocity(Right_M, Max_S);
-%wb_motor_set_position(Left_M_B, inf);
-%wb_motor_set_velocity(Left_M_B, Max_S);
-%wb_motor_set_position(Right_M_B, inf);
-%wb_motor_set_velocity(Right_M_B, Max_S);
+
   drawnow;
   
 end
