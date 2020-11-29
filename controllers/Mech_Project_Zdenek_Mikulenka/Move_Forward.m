d5351562b2cdad4f60da37a96bec882f53b995f8
Ondřej Mikulenka <228580@vutbr.cz>
2020-11-29 21:14:47 +0100

    Update pohybu na 3 nohy
    Když si spustíš simulaci, tak podle té předlohy si myslím, že by neměla být složitá na pochopení ta fce. Takže pokud by si to viděl, tak by si to mohl dodát, aby dal aspoń krok svýma nohama. pro dalří trojici bych asi spíše vytvořil dalši funki obdobnou té první a vložil bych ji jen pod to.
    Pak by jsme klidně mohli do přivolávací fce vložit proměnnou (počet kroků). Pak už by jsme měli aspoň chodícího Hexapoda, což by bylo mega.

--- controllers/Mech_Project_Zdenek_Mikulenka/Move_Forward.m
+++ controllers/Mech_Project_Zdenek_Mikulenka/Move_Forward.m
@@ -29,18 +29,23 @@ function  Move_Forward(Legs, Max_S)
 % wb_motor_set_position(Legs(5, 3), -2);
 % wb_motor_set_velocity(Legs(5, 3), Max_S);
 number = 20;
-shoulder_vec = linspace(-1, 1, number);
-base_vec = linspace(-0.55, 0.55, number);
-knee_vec = linspace(-2, 2, number);
+shoulder_vec = linspace(1, 1.8, number);
+base_vec = linspace(-0.6, 0.6, number);
+knee_vec = linspace(-2.4, -2, number);
 for i = 1 : number
 %     wb_motor_set_velocity(Legs, Max_S);
     
     wb_motor_set_position(Legs(1, 2), shoulder_vec(i));
     wb_motor_set_position(Legs(2, 2), shoulder_vec(i));
     wb_motor_set_position(Legs(3, 2), shoulder_vec(i));
-%     wb_motor_set_position(Legs((1:3), 1), base_vec(i));
-%     wb_motor_set_position(Legs((1:3), 3), knee_vec(i));
-%     
+    
+    wb_motor_set_position(Legs(1, 1), base_vec(i));
+    wb_motor_set_position(Legs(2, 1), -base_vec(i));
+    wb_motor_set_position(Legs(3, 1), base_vec(i));
+    
+    wb_motor_set_position(Legs(1, 3), knee_vec(i));
+    wb_motor_set_position(Legs(2, 3), knee_vec(i));
+    wb_motor_set_position(Legs(3, 3), knee_vec(i));
 %     
 %     wb_motor_set_position(Legs((4:6), 2), shoulder_vec(i));
 %     wb_motor_set_position(Legs((4:6), 1), base_vec(i));
