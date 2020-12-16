adc091e3429744b39c1880ba9fc7b52eb7b025c9
Ondřej Mikulenka <228580@vutbr.cz>
2020-12-16 15:05:26 +0100

    Dokončení hexapoda
    Přidína chůze diagonálně (vpřed). Změna prostředí a mírná aktualizace kódu. Předělání kláves ze šipek na WASD QE [SPACE]. Přidání překážky, přes kterou hexapod přejde ve vysoké poloze.

--- controllers/Mech_Project_Zdenek_Mikulenka/Mech_Project_Zdenek_Mikulenka.m
+++ controllers/Mech_Project_Zdenek_Mikulenka/Mech_Project_Zdenek_Mikulenka.m
@@ -1,8 +1,8 @@
 
 
-TIME_STEP = 300;
+TIME_STEP = 150;
   Max_S = 10;
-
+% Zakladni offsety
 % offset Base f, m, r [rad] = 0.6, 0, -0.6 
 % offset Shdr u, d [rad] = 1, 0.8  
 % offset Knee u, d [rad] = -2.35, -2.4   
@@ -43,26 +43,31 @@ while wb_robot_step(TIME_STEP) ~= -1
           wb_motor_set_position(Legs(i, 3), knee_up);
         end
         n = 2;
-        drawnow;
         
       case 2
         for i = 1:3
-        if key == WB_KEYBOARD_UP
+        if key == 87
           wb_motor_set_position(Legs(i, 1), offset(i)+(-1)^(i+1)*0.25);
           wb_motor_set_position(Legs(3+i, 1), offset(4-i)+(-1)^(i+1)*0.25);
-        elseif key == WB_KEYBOARD_DOWN
+        elseif key == 83
           wb_motor_set_position(Legs(i, 1), offset(i)-(-1)^(i+1)*0.25);
           wb_motor_set_position(Legs(3+i, 1), offset(4-i)-(-1)^(i+1)*0.25);  
-        elseif key == WB_KEYBOARD_RIGHT 
+        elseif key == 68 
           wb_motor_set_position(Legs(i, 1), offset(i)+(-1)*0.25);
           wb_motor_set_position(Legs(3+i, 1), offset(4-i)+(+1)*0.25);
-        elseif key == WB_KEYBOARD_LEFT 
+        elseif key == 65
           wb_motor_set_position(Legs(i, 1), offset(i)-(-1)*0.25);
           wb_motor_set_position(Legs(3+i, 1), offset(4-i)-(+1)*0.25);
+        elseif key == 81
+          wb_motor_set_position(Legs(i, 1), offset(i)+(-1)^(i+1)*0.25+0.2);
+          wb_motor_set_position(Legs(3+i, 1), offset(4-i)+(-1)^(i+1)*0.25-0.2);
+        elseif key == 69
+          wb_motor_set_position(Legs(i, 1), offset(i)+(-1)^(i+1)*0.25-0.2);
+          wb_motor_set_position(Legs(3+i, 1), offset(4-i)+(-1)^(i+1)*0.25+0.2);
         end
         end
         n = 3;
-        drawnow;
+        % Pro orientaci pri pripadne uprave kodu
         %wb_motor_set_position(Legs(1, 1), -0.6+0.25); %zadní
         %wb_motor_set_position(Legs(2, 1), 0-0.25); %prostřední
         %wb_motor_set_position(Legs(3, 1), 0.6+0.25); %přední
@@ -76,7 +81,6 @@ while wb_robot_step(TIME_STEP) ~= -1
           wb_motor_set_position(Legs(i, 3), knee_down);
         end
         n = 4;
-        drawnow;
         
       case 4
         for i = 4:6
@@ -84,26 +88,30 @@ while wb_robot_step(TIME_STEP) ~= -1
           wb_motor_set_position(Legs(i, 3), knee_up);
         end
         n = 5;
-        drawnow;
         
       case 5
         for i = 4:6
-        if key == WB_KEYBOARD_UP
+        if key == 87
           wb_motor_set_position(Legs(i-3, 1), offset(i-3)+(-1)^(i+1)*0.25);
           wb_motor_set_position(Legs(i, 1), offset(7-i)+(-1)^(i+1)*0.25);        
-        elseif key == WB_KEYBOARD_DOWN
+        elseif key == 83
           wb_motor_set_position(Legs(i-3, 1), offset(i-3)-(-1)^(i+1)*0.25);
           wb_motor_set_position(Legs(i, 1), offset(7-i)-(-1)^(i+1)*0.25);
-        elseif key == WB_KEYBOARD_RIGHT 
+        elseif key == 68 
           wb_motor_set_position(Legs(i-3, 1), offset(i-3)+(+1)*0.25);
           wb_motor_set_position(Legs(i, 1), offset(7-i)+(-1)*0.25);
-        elseif key == WB_KEYBOARD_LEFT 
+        elseif key == 65 
           wb_motor_set_position(Legs(i-3, 1), offset(i-3)-(+1)*0.25);
           wb_motor_set_position(Legs(i, 1), offset(7-i)-(-1)*0.25);
+          elseif key == 81
+          wb_motor_set_position(Legs(i-3, 1), offset(i-3)+(-1)^(i+1)*0.25-0.2);
+          wb_motor_set_position(Legs(i, 1), offset(7-i)+(-1)^(i+1)*0.25+0.2);
+          elseif key == 69
+          wb_motor_set_position(Legs(i-3, 1), offset(i-3)+(-1)^(i+1)*0.25+0.2);
+          wb_motor_set_position(Legs(i, 1), offset(7-i)+(-1)^(i+1)*0.25-0.2);
         end  
         end
         n = 6;
-        drawnow;
 
       case 6
         for i = 4:6
@@ -111,7 +119,6 @@ while wb_robot_step(TIME_STEP) ~= -1
           wb_motor_set_position(Legs(i, 3), knee_down);
         end
         n = 1;
-        drawnow;
       end
   else
     for i = 1:3
@@ -128,7 +135,7 @@ while wb_robot_step(TIME_STEP) ~= -1
     shdr_down =-0.2; 
     knee_up = -0.95;
     knee_down =-1;
-    p = 1
+    p = 1;
     for i = 1:6
           wb_motor_set_position(Legs(i, 2), shdr_down);
           wb_motor_set_position(Legs(i, 3), knee_down);
@@ -138,7 +145,7 @@ while wb_robot_step(TIME_STEP) ~= -1
     shdr_down =0.8; 
     knee_up = -2.35;
     knee_down =-2.4;
-    p = 0
+    p = 0;
     for i = 1:6
           wb_motor_set_position(Legs(i, 2), shdr_down);
           wb_motor_set_position(Legs(i, 3), knee_down);
